c4c7e44a7ded028e2ede20603f0bb08d253ef350
|_ VV F <lao58qiuqiule@gmail.com>
2025-10-07 00:02:32 -0700

    feat: deploy to GitHub Page

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -1,13 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+  // Set base path for GitHub Pages deployment
+  base: process.env.NODE_ENV === "production" ? "/React19Learn/" : "/",
   plugins: [
     react({
       babel: {
-        plugins: [['babel-plugin-react-compiler']],
+        plugins: [["babel-plugin-react-compiler"]],
       },
     }),
   ],
-})
+  build: {
+    outDir: "dist",
+    sourcemap: true,
+  },
+});
